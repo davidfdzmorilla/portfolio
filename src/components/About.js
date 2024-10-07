aa58c37f487407cec3a56b3340d1ebb6cf878c70
David Fernandez <david.fernandez@gestiondeformacion.es>
2024-10-07 21:09:39 +0200

    Test deploy workflow

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ export default function About() {
         <article id='about-page'>
             <article className='content'>
                 <h2>Este soy yo</h2>
-                <p>Soy un desarrollador full stack con habilidades en HTML, CSS, JavaScript, Node.js, MySQL, PHP, Python, Docker, Nginx y Traefik. Mi experiencia en el desarrollo web me permite abordar proyectos de manera eficiente y asegurar la escalabilidad, disponibilidad y seguridad de las aplicaciones en entornos de producción. Busco constantemente mejorar mis habilidades y estar actualizado en las últimas tendencias y tecnologías. Soy dedicado y apasionado con un enfoque en la calidad y la atención al detalle en cada proyecto que entrego. Estoy comprometido a entregar soluciones de alta calidad y ampliar mis conocimientos.</p>
+                <p>Soy un desarrollador full stack con habilidades en HTM, CSS, JavaScript, Node.js, MySQL, PHP, Python, Docker, Nginx y Traefik. Mi experiencia en el desarrollo web me permite abordar proyectos de manera eficiente y asegurar la escalabilidad, disponibilidad y seguridad de las aplicaciones en entornos de producción. Busco constantemente mejorar mis habilidades y estar actualizado en las últimas tendencias y tecnologías. Soy dedicado y apasionado con un enfoque en la calidad y la atención al detalle en cada proyecto que entrego. Estoy comprometido a entregar soluciones de alta calidad y ampliar mis conocimientos.</p>
             </article>
             <section className="avatar-container">
                 <div className="face-me" />
