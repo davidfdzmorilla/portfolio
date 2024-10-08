c9969a752a93ae75c74d0818175101cb523aea59
David Fernandez <david.fernandez@gestiondeformacion.es>
2024-10-08 16:14:10 +0200

    Added swiper slider

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,33 +1,82 @@
 import { AiFillGithub, AiOutlineLink, AiOutlineVideoCamera } from 'react-icons/ai';
-import projectsList from '../data/works.json'
+import projectsList from '../data/works.json';
+import './Works.css';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 
-import "./Works.css"
-
-
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 
 export default function Works() {
-
     return (
         <article id='works-page'>
             <h2>Proyectos</h2>
-            <section className="works-container">
-                {projectsList?.map(project => {
-                    return (
-                        <article key={Math.random()} id={project.urlVideo} className="work-card">
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={window.innerWidth < 768 ? 1 : 2}
+                spaceBetween={20} // Mostrar más de una tarjeta en pantallas más grandes
+                coverflowEffect={{
+                    rotate: 30,
+                    stretch: 10,
+                    depth: 200,
+                    modifier: 1,
+                    slideShadows: false,
+                }}
+                pagination={{ clickable: true }}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                speed={1000}
+                modules={[EffectCoverflow, Pagination, Autoplay]}
+                className="mySwiper"
+            >
+                {projectsList?.map(project => (
+                    <SwiperSlide key={project.title}>
+                        <article id={project.urlVideo} className="work-card">
                             <div className="header-card">
                                 <span>{project.title}</span>
                                 <div className="links-container">
-                                    <a href={project.urlRepo} target='_blank' rel="noreferrer nopener" className="logo"><AiFillGithub /></a>
-                                    {project.link.length > 1 ? <a href={project.link} target='_blank' rel="noreferrer nopener" className="logo"><AiOutlineLink /></a> : ''}
-                                    {project.urlVideo.length > 1 ? <a href={project.urlVideo} target='_blank' rel="noreferrer nopener" className="logo"><AiOutlineVideoCamera /></a> : ''}
+                                    <a
+                                        href={project.urlRepo}
+                                        target='_blank'
+                                        rel="noreferrer noopener"
+                                        className="logo"
+                                    >
+                                        <AiFillGithub />
+                                    </a>
+                                    {project.link && (
+                                        <a
+                                            href={project.link}
+                                            target='_blank'
+                                            rel="noreferrer noopener"
+                                            className="logo"
+                                        >
+                                            <AiOutlineLink />
+                                        </a>
+                                    )}
+                                    {project.urlVideo && (
+                                        <a
+                                            href={project.urlVideo}
+                                            target='_blank'
+                                            rel="noreferrer noopener"
+                                            className="logo"
+                                        >
+                                            <AiOutlineVideoCamera />
+                                        </a>
+                                    )}
                                 </div>
                             </div>
                             <img src={project.urlImage} alt='project' />
                             <p>{project.description}</p>
-                        </article>)
-                })}
-            </section>
+                        </article>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </article>
-    )
+    );
 }
